93748f1206ef17c83f77b2dc01c2e3f43aa31c87
Dan Sullivan <daniel.f.sullivan@gmail.com>
2021-03-22 06:12:39 +0000

    Add project files.

---
+++ BinaryDigits/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace BinaryDigits
+{
+    class Program
+    {
+        static string margin = "       ";
+
+        static void PrintBitValues()
+        {
+            int nrBits = 8;
+
+            // PRINT '1's
+            string output = margin;
+            for (int i = 0; i < nrBits; i++) output += "1";
+            Console.WriteLine( $"\n{output}\n{margin}[Enter]\n" );
+            Console.ReadLine();
+
+            // PRINT '1's and exponents and place values
+            output = margin;
+            for (int i = 0; i < nrBits; i++) output += Convert.ToString(nrBits - i- 1);
+            output += $"\n{margin}";
+            for (int i = 0; i < nrBits; i++) output += "1";
+            output += "\n\n";
+            for (int digit = 0; digit < nrBits; digit++)
+            {
+                for (int j = 0; j < nrBits - digit; j++) output += " ";
+                output += $"2^{digit} = {Math.Pow(2,digit)}\n";
+            }
+            Console.WriteLine($"\n{output}\n{margin}[Enter]\n");
+            Console.ReadLine();
+        }
+
+        static void PrintBitValues(int number)
+        {
+            string outputString = margin + GetBinaryString(number) + "\n";
+
+            int numDigits = Convert.ToInt32(Math.Log2(number));
+            for (int digit = numDigits; digit >= 0 ; digit--)
+            {
+                int thisDigit = Convert.ToInt32(Math.Pow(2, digit));
+                if ( number >= thisDigit)
+                {
+                    string leader = "";
+                    for (int i = 0; i < numDigits - digit; i++) leader += " ";
+                    outputString += $"{margin}{leader}^ + {thisDigit}\n";
+                    number -= thisDigit;
+                }
+            }
+            Console.WriteLine(outputString);
+        }
+
+        static string GetBinaryString(int x)
+        {
+            string binaryString = "";
+            int numDigits = Convert.ToInt32(Math.Log2(x));
+            for (int i = numDigits; i >= 0; i--)
+            {
+                if( x < Math.Pow(2,i)) binaryString += "0";
+                else
+                {
+                    binaryString += '1';
+                    x -= Convert.ToInt32(Math.Pow(2, i));
+                }
+            }
+            return binaryString;
+        }
+
+        static int GetNextValue()
+        {
+            Console.Write($"{margin}Enter integer value to Convert (or 0): ");
+            return Int32.Parse(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            PrintBitValues();
+
+            int n;
+            while ( true )
+            {
+                if( (n = GetNextValue()) == 0) break;
+                PrintBitValues(n);
+            }
+        }
+    }
+}
---
+++ Counter/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace Counter
+{
+    class Program
+    {
+        static int n = 1;
+        static int waitTime = 500;  // msec
+        static string margin = "      ";
+
+        static bool showDec = true;
+        static bool showHex = true;
+        static bool showBin = true;
+
+        static int GetNextValue()
+        {
+            Console.Write($"\n{margin}Count to (or 0): ");
+            return Int32.Parse(Console.ReadLine());
+        }
+        static void Counter(int n)
+        {
+            Console.WriteLine( margin
+                    + (showHex ? "  (hex)  " : string.Empty)
+                    + (showDec ? "  (dec)  " : string.Empty)
+                    + (showBin ? "  (bin)  " : string.Empty) );
+
+            string countDisplay;
+
+            for (int i = 0; i <= n; i++)
+            {
+                countDisplay = string.Empty;
+                countDisplay += $"{margin}{ (showHex ? Convert.ToString(i,16) : string.Empty),4 }";
+                countDisplay += $"{margin}{ (showDec ? Convert.ToString(i,10) : string.Empty),4 }";
+                countDisplay += $"{margin}{ (showBin ? Convert.ToString(i,2) : string.Empty),4 }";
+
+                string backSpaces = "";
+                for (int j = 0; j < countDisplay.Length; j++) backSpaces += "\b";
+
+                Console.Write($"{backSpaces}{countDisplay}");
+                if (waitTime > 0) Thread.Sleep(waitTime);
+                else Console.ReadKey();
+            }
+        }
+        static void ConfigureApp()
+        {
+            int wait;
+            Console.Write($"\n\nDelay: ([{waitTime}]) msec: ");
+            if (Int32.TryParse(Console.ReadLine(), out wait)) waitTime = wait;
+            Console.WriteLine($"{margin}Delay: {(waitTime > 0 ? waitTime : "step")}\n");
+
+            Console.Write("show hexadecimal counter ([Y],n) ");
+            if (Console.ReadLine().ToLower().StartsWith('n')) showHex = false;
+            Console.Write("show decimal counter ([Y],n) ");
+            if (Console.ReadLine().ToLower().StartsWith('n')) showDec = false;
+            Console.Write("show binary counter ([Y],n) ");
+            if (Console.ReadLine().ToLower().StartsWith('n')) showBin = false;
+
+            n = GetNextValue();
+        }
+
+        static void Main(string[] args)
+        {
+            while (n > 0)
+            {
+                ConfigureApp();
+                Counter(n);
+            }
+        }
+    }
+}
